77b78b84f07444cfe5ea697ab648cd96f2b3a921
Zhenya Ruban <ruban.z1@mail.ru>
2022-04-09 18:28:04 +0300

    add random value

--- js/main.js
+++ js/main.js
@@ -25,14 +25,23 @@ function handelBoxClick(event) {
 function renderBox() {
     $bgGame.innerHTML = '';
     const box = document.createElement('div');
-    box.style.height = box.style.width = '50px';
+    const boxSize = getRandom(20, 80);
+    const gameSize = $bgGame.getBoundingClientRect();
+    const maxTop = gameSize.height - boxSize;
+    const maxLeft = gameSize.width - boxSize;
+
+    box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#000';
-    box.style.top = '50px';
-    box.style.left = '70px';
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', true);
 
     $bgGame.insertAdjacentElement("afterbegin", box);
     // $bgGame.append(box);
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
