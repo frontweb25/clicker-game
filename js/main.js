ac9b71de8675902e746b2352cb981dd11acca347
Zhenya Ruban <ruban.z1@mail.ru>
2022-04-09 17:51:07 +0300

    render box

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 const $start = document.querySelector('#start');
 const $bgGame = document.querySelector('#game');
 
+let score = 0;
+
 $start.addEventListener('click', startGame);
+$bgGame.addEventListener('click', handelBoxClick);
 
 function startGame() {
     $start.classList.add('hide');
@@ -11,6 +14,25 @@ function startGame() {
     renderBox();
 }
 
+
+function handelBoxClick(event) {
+    if(event.target.dataset.box) {
+      score++;
+      renderBox();
+    }
+}
+
 function renderBox() {
-    
+    $bgGame.innerHTML = '';
+    const box = document.createElement('div');
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '70px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', true);
+
+    $bgGame.insertAdjacentElement("afterbegin", box);
+    // $bgGame.append(box);
 }
