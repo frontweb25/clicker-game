089b6d392e4c1df040f0122c2bb7645eefac8cf9
Zhenya Ruban <ruban.z1@mail.ru>
2022-04-09 17:13:54 +0300

    start game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log()
+
+const $start = document.querySelector('#start');
+const $bgGame = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+
+function startGame() {
+    $start.classList.add('hide');
+    $bgGame.style.backgroundColor = '#fff';
+
+    renderBox();
+}
+
+function renderBox() {
+    
+}
