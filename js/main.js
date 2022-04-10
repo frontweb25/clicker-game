a9fd8ac19835fadd3f2c13428f146d62b312c4b9
Zhenya Ruban <ruban.z1@mail.ru>
2022-04-10 16:40:39 +0300

    add result

--- js/main.js
+++ js/main.js
@@ -1,22 +1,60 @@
+// 1)создать переменную $start и подкючить кнопку 
+// 2)создать переменную $bgGame и подключить тело квадрата
+// 3)создать переменную $time и подключить время
+
+//4) создать функцию startGame 
+//5) при клике на $start она должна скрываться и меняться background $bgGame 
+//6) создать функцию renderBox 
+//7) внутрь renderBox создать квадрат 50px. позиционировать его.изменить цвет
+// задать top 50 left 70  квадрату. добавить квадрат на страницу 
+//8) создать функию handelBoxClick повесить слушателя addEventListener 
+//9) сделать деоегирование событий для $bgGame внуцтри функции handelBoxClick
+// dataset.box подсказка 
+//10) создать переменную score = 0 и поместить в handelBoxClick 
+//должна запускаться renderBox
+//11) $bgGame.innerHTML = '' добавить в renderBox
+//12)создать функцию getRandom(min, max) возвращает 
+//Math.floor(Math.random() * (max - min) + min)
+//13) в renderBox создать переменную boxSize и присвоить getRandom()
+//14) присвоить boxSize квадрату box
+//15) в функции renderBox создать переменную gameSize 
+// и присвоить ей $bgGame.getBoundingClientRect();
+//создать переменную maxTop = gameSize.height - boxSize
+// top и left = присвозить getRandom(0, maxTop) + px
+//16) в startGame создать переменную interval с SetInterval
+//создать переменную time и присвоитm parseFloat($time.textContent)
+//если time <=0 тогда конец игры если нет то $time.textContent = time - 0.1
+//создать функцию endGame
+//создать переменную isGameStarted cо значение false
+//в переменную startGame поместить переменную isGameStarted = true
+//в handelBoxClick сделать проверку если игра не !isGameStarted тогда return
+
 
 const $start = document.querySelector('#start');
 const $bgGame = document.querySelector('#game');
 const $time = document.querySelector('#time');
-
-$start.addEventListener('click', startGame);
-$bgGame.addEventListener('click', handelBoxClick);  
+const $result = document.querySelector('#result');
+const $timeHeader = document.querySelector('#time-header');
+const $resultHeader = document.querySelector('#result-header');
 
 let score = 0;
 let isGameStarted = false;
 
 
-function startGame() {
-    isGameStarted = true;
-    $start.classList.add('hide')
-    $bgGame.style.backgroundColor = '#fff';
+$start.addEventListener('click', startGame);
+$bgGame.addEventListener('click', handelBoxClick);
 
+function startGame() {
+    score = 0;
+    setGameTime();
+    $timeHeader.classList.remove('hide');
+    $resultHeader.classList.add('hide');
+
+    isGameStarted = true;
+    $start.classList.add('hide');
+    $bgGame.style.backgroundColor = '#fff';
     const interval = setInterval(function() {
-        const time =  parseFloat($time.textContent);
+        const time = parseFloat($time.textContent);
         if(time <= 0) {
             clearInterval(interval);
             endGame();
@@ -28,22 +66,26 @@ function startGame() {
     renderBox();
 }
 
+function setGameScore() {
+    $result.textContent = score.toString();
+}
+
+function setGameTime() {
+    let time = 5;
+    $time.textContent = time.toFixed(1);
+}
 
 function endGame() {
     isGameStarted = false;
-}
+    setGameScore();
 
-function handelBoxClick(event) {
-    if(!isGameStarted) {
-        return;
-    }
-    if(event.target.dataset.box) {
-        score++;
-        renderBox();
-        
-    }
+    $start.classList.remove('hide');
+    $bgGame.style.backgroundColor = '#ccc';
+    $bgGame.innerHTML = '';
+    $timeHeader.classList.add('hide');
+    $resultHeader.classList.remove('hide');
+    
 }
-
 
 function renderBox() {
     $bgGame.innerHTML = '';
@@ -54,15 +96,23 @@ function renderBox() {
     const maxLeft = gameSize.width - boxSize;
 
     box.style.height = box.style.width = boxSize + 'px';
+    box.style.background = '#000';
+    box.style.cursor = 'pointer';
     box.style.position = 'absolute';
-    box.style.backgroundColor = '#000';
     box.style.top = getRandom(0, maxTop) + 'px';
     box.style.left = getRandom(0, maxLeft) + 'px';
-    box.style.cursor = 'pointer';
-    box.setAttribute('data-box', true); 
-    // $bgGame.insertAdjacentElement('afterbegin', box);
+    box.setAttribute('data-box', true);
     $bgGame.append(box);
-    
+}
+
+function handelBoxClick(event) {
+    if(!isGameStarted) {
+        return;
+    }
+    if(event.target.dataset.box) {
+        score++;
+        renderBox();
+    }
 }
 
 function getRandom(min, max) {
